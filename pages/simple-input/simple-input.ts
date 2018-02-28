e6bf95d3aac0ee245c85bacbdb10cb9531f99d13
baldore <acornssn@gmail.com>
2018-02-28 13:31:14 -0500

    Add simple-input example

--- pages/simple-input/simple-input.ts
+++ pages/simple-input/simple-input.ts
@@ -1,1 +1,28 @@
-console.log('fooo')
+import { Observable } from 'rxjs'
+
+if (module.hot) {
+  module.hot.dispose(() => {
+    window.location.reload()
+  })
+}
+
+const selectors = {
+  input: '#my-input',
+  content: '#my-content',
+}
+
+const dom = {
+  input: document.querySelector(selectors.input) as HTMLInputElement,
+  content: document.querySelector(selectors.content) as HTMLElement,
+}
+
+const input$: Observable<Event> = Observable.create(observer => {
+  dom.input.addEventListener('input', e => observer.next(e))
+})
+
+// Simpler!
+// const input$ = Observable.fromEvent(dom.input, 'input')
+
+input$.subscribe(e => {
+  dom.content.innerText = dom.input.value
+})
